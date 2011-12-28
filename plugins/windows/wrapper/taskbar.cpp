93b2b0536d7067de809cf4a42fa00af4f2d39360
Sidorov Aleksey <sauron@citadelspb.com>
2011-12-28 17:50:55 +0000

    Add copyrights

--- plugins/windows/wrapper/taskbar.cpp
+++ plugins/windows/wrapper/taskbar.cpp
@@ -1,3 +1,28 @@
+/****************************************************************************
+ *  taskbar.cpp
+ *
+ *  Copyright (c) 2011 by Sidorov Aleksey <gorthauer87@ya.ru>
+ *  Copyright (c) 2011 Ivan Vizir <define-true-false@yandex.com>
+ *
+ ***************************************************************************
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 3 of the License, or (at your option) any later version.
+
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+
+ * You should have received a copy of the GNU Lesser General
+ * Public License along with this library; if not, write to the
+ * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
+ * Boston, MA 02110-1301 USA
+ *
+*****************************************************************************/
+
 #include "taskbar.h"
 #include <ShObjIdl.h>
 #include "jumplistsmanager.h"
