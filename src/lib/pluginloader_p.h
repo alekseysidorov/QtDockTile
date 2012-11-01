37d7423b3594eaf5fc8370fe2214bdca9d93eb04
Aleksey Sidorov <gorthauer87@ya.ru>
2012-11-01 20:12:53 +0400

    License update

--- src/lib/pluginloader_p.h
+++ src/lib/pluginloader_p.h
@@ -1,3 +1,27 @@
+/****************************************************************************
+**
+** QtDockTile - crossplatform for the work this docks 
+**
+** Copyright © 2012 Aleksey Sidorov <gorthauer87@ya.ru>
+**
+*****************************************************************************
+**
+** $QTDOCKTILE_BEGIN_LICENSE$
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU Lesser General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see http://www.gnu.org/licenses/.
+** $QTDOCKTILE_END_LICENSE$
+**
+****************************************************************************/
 #ifndef PLUGINLOADER_H
 #define PLUGINLOADER_H
 #include <QObject>
@@ -41,3 +65,4 @@ Q_INLINE_TEMPLATE QList<T*> PluginLoader::instances()
 }
 
 #endif // PLUGINLOADER_H
+
--- src/lib/qtdockmanager_p.h
+++ src/lib/qtdockmanager_p.h
@@ -1,26 +1,28 @@
 /****************************************************************************
- *  qtdockmanager_p.h
- *
- *  Copyright (c) 2011 by Sidorov Aleksey <gorthauer87@ya.ru>
- *
- ***************************************************************************
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 3 of the License, or (at your option) any later version.
-
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
-
- * You should have received a copy of the GNU Lesser General
- * Public License along with this library; if not, write to the
- * Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor,
- * Boston, MA 02110-1301 USA
- *
-*****************************************************************************/
+**
+** QtDockTile - crossplatform for the work this docks 
+**
+** Copyright © 1301 USA
+** Copyright © 2012 Sidorov Aleksey <gorthauer87@ya.ru>
+**
+*****************************************************************************
+**
+** $QTDOCKTILE_BEGIN_LICENSE$
+** This program is free software: you can redistribute it and/or modify
+** it under the terms of the GNU Lesser General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** This program is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+** See the GNU Lesser General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with this program.  If not, see http://www.gnu.org/licenses/.
+** $QTDOCKTILE_END_LICENSE$
+**
+****************************************************************************/
 
 /*
   * This file is a part of private API
@@ -72,3 +74,4 @@ private:
 };
 
 #endif // QTDOCKMANAGER_P_H
+
